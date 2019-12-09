4edbff2bc29728159cdd467a07a2d6ec5d2907b5
shiyanfei5 <13218016202@163.com>
2019-12-09 23:05:41 +0800

    （*) 完成第二章

---
+++ src/main/java/ex03/pymont/connector/http/HttpConnector.java
@@ -0,0 +1,45 @@
+package ex03.pymont.connector.http;
+
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * connector支持多线程，要实现Runnable接口
+ * 其只处理socket通信的启停
+ */
+public class HttpConnector implements Runnable{
+
+
+    private boolean stopped;    //是否停止
+    private String scheme = "http";
+
+
+    /**
+     * connector将socket流 交给processor
+     */
+    @Override
+    public void run() {
+        ServerSocket serverSocket = null;
+        int port = 8080;
+        try{
+            serverSocket = new ServerSocket(port,1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e){
+            e.printStackTrace();
+            System.exit(1); //若出现问题则直接退出
+        }
+
+        while(!stopped){
+            try{
+                Socket socket = serverSocket.accept();
+                HttpProcessor httpProcessor = new HttpProcessor(this);
+                httpProcessor.process(socket);
+            } catch (IOException e){
+                e.printStackTrace();
+                continue;
+            }
+        }
+    }
+}
---
+++ src/main/java/ex03/pymont/connector/http/HttpProcessor.java
@@ -0,0 +1,33 @@
+package ex03.pymont.connector.http;
+
+import java.net.Socket;
+
+public class HttpProcessor {
+
+
+    //---------------------------实例属性-------------------------
+    private HttpConnector connector;    //关联的connector
+
+
+    public HttpProcessor(HttpConnector connector) {
+        this.connector = connector;
+    }
+
+    //******开始最麻烦的 http请求解析************
+    public void process(Socket socket){
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
